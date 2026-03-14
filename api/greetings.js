e2966512457a36c59912c6f3096603fde562eaff
cryptogore <141570530+cryptogore@users.noreply.github.com>
2026-03-14 14:25:40 +0700

    Update greetings.js

--- api/greetings.js
+++ api/greetings.js
@@ -1,14 +1,14 @@
-export default function handler(req, res){
+export default function handler(req,res){
 
-  const greetings = [
-    "yo traveler 👋 I'm Siggy",
-    "psst... hey explorer 🐾",
-    "welcome wanderer ✨",
-    "hey there stranger 😼",
-    "ah... another curious soul 🔮"
-  ];
+const greetings=[
+"yo traveler 👋 I'm Siggy",
+"psst... hey explorer 🐾",
+"welcome wanderer ✨",
+"hey there 😼",
+"another curious soul 🔮"
+];
 
-  const g = greetings[Math.floor(Math.random()*greetings.length)];
+const g=greetings[Math.floor(Math.random()*greetings.length)];
 
-  res.status(200).json({ greeting: g });
+res.status(200).json({greeting:g});
 }
